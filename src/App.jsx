69db2c3aa2b5c997c833276d42e43cfa0aa03ddc
Rednata <maknatosh@gmail.com>
2024-01-15 14:21:58 +0700

    style for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from './views/Header/Header.jsx';
-import { Footer } from './views/Footer/Footer.jsx';
+// import { Footer } from './views/Footer/Footer.jsx';
 
 export const App = () => (
   <>
     <Header />
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,17 @@
+import s from './Navigation.module.scss';
+import CartIcon from './bag.svg?react';
+import HeartIcon from './heart.svg?react';
+
 export const Navigation = () => (
-  <div>Navigation
-  </div>
+  <nav className={s.navigation}>
+    <a className={s.link} href="/favotite">
+      <span className={s.text}>Избранное</span>
+      <HeartIcon className={s.svg}/>
+    </a>
+    <a className={s.link} href="/cart">
+      <span className={s.text}>Корзина</span>
+      <span>(0)</span>
+      <CartIcon className={s.svg}/>
+    </a>
+  </nav>
 );
